b6121e79b5f8f459abf596bf5d02646d21d40b40
jaseg <s@jaseg.de>
2013-02-16 23:47:01 +0100

    lmap unit tests working

--- lmap/lmap.py
+++ lmap/lmap.py
@@ -1,13 +1,30 @@
 
 import ldap, itertools
 
-class LMAP(dict):
+def _child_name(dn):
+	return dn.partition(',')[0].partition('=')[2]
+
+def _compmod(new, old):
+	modlist = []
+	for k in new.keys():
+		if k not in old:
+			modlist.append((ldap.ldapmod.ADD, k, new[k]))
+		elif new[k] != old[k]:
+			modlist.append((ldap.ldapmod.REPLACE, k, new[k]))
+	for k in old.keys():
+		if k not in new:
+			modlist.append((ldap.ldapmod.DELETE, k, None))
+	return modlist
+
+class lmap(dict):
 #Object infrastructure
-	def __init__(self, ldap=None, dn='', timeout=-1):
+	def __init__(self, attrs=None, dn='', ldap=None, timeout=-1):
 		self._ldap = ldap
 		self.timeout = timeout
 		self.dn = dn
-		self._log = None
+		self._rollback_state = None
+		self._cached_attrs = attrs
+		self._cached_children = None
 	
 #Transaction handling
 	def __enter__(self):
@@ -20,34 +37,54 @@ class LMAP(dict):
 			self.commit()
 
 	def start_transaction(self):
-		self.fetch()
-		self._log = {}
+		self.fetch_attrs()
+		self._rollback_state = self._cached_attrs.copy()
 
 	def rollback(self):
-		self._log = None
+		self._cached_attrs = self._rollback_state
 
 	def commit(self):
-		mod = lambda k,v: ldap.MOD_DELETE if v is None else (ldap.MOD_REPLACE if k in self._cached_attrs: else ldap.MOD_ADD)
-		modlist = [ (mod(k,v), k, v) for k,v in self._log.items() ]
-		self._ldap.modify(self.dn, modlist) #FIXME the ldap lib does not supports timeouts here
-		self._log = None
+		self._ldap.modify(self.dn, _compmod(self._cached_attrs, self._rollback_state)) #FIXME apparently, the ldap lib does not support timeouts here
+		self._rollback_state = None
+		#FIXME the following line seems sub-optimal and even unnecessary.
+		self.fetch_attrs()
 	
 #Attribute access
 	def fetch_attrs(self):
-		_, self._cached_attrs = _, rv = self._ldap.search(self.dn, ldap.SCOPE_BASE, timeout=self.timeout)[0]
+		_, self._cached_attrs = _, rv = self._ldap.search(self.dn, ldap.Scope.BASE, timeout=self.timeout)[0]
 		return rv
 
 	def keys(self):
 		return self._cached_attrs or self.fetch_attrs()
 
 	def __setitem__(self, name, value):
+		#FIXME prevent self['dn'] and self.dn from getting out of sync?
+		if not self._cached_attrs:
+			self.fetch_attrs()
+		if not self._rollback_state:
+			op = ldap.ldapmod.ADD if name not in self._cached_attrs.keys() else ldap.ldapmod.REPLACE
+			self._ldap.modify(self.dn, [(op, name, value)])
 		self._cached_attrs[name] = value
 
 	def __getitem__(self, name):
-		self._cached_attrs.__getitem__(name)
+		if not self._cached_attrs:
+			self.fetch_attrs()
+		return self._cached_attrs[name]
+	
+	def __delitem__(self, name):
+		if not self._cached_attrs:
+			self.fetch_attrs()
+		del self._cached_attrs[name]
+		if not self._rollback_state:
+			self._ldap.modify(self.dn, [(ldap.ldapmod.DELETE, name, None)])
+	
+	def __contains__(self, item):
+		if not self._cached_attrs:
+			self.fetch_attrs()
+		return item in self._cached_attrs
 
 	def __missing__(self, name):
-		self.fetch()
+		self.fetch_children()
 		if name in self:
 			return name
 		raise KeyError(name+' not found')
@@ -56,36 +93,54 @@ class LMAP(dict):
 		pass #handle assignment of dicts as attrs
 
 #Tree operations
-	def add(self, dn):
-		""" Add this entry under the given dn """
+	def add(self, rdn, entry):
+		""" Add an entry under this entry with the given rdn """
+		if _child_name(rdn) in self.children().keys():
+			raise ValueError('There already is an entry at this position of the LDAP tree, or there is a naming collision.')
+			#FIXME document these "naming collisions"
+		if entry.dn:
+			raise ValueError('The entry\'s dn is set, this means the entry already is part of an LDAP tree.')
+		dn = '{},{}'.format(rdn, self.dn)
+		entry.dn = dn
+		entry._ldap = self._ldap
+		self._ldap.add(dn, entry._cached_attrs)
+		self._cached_children[_child_name(rdn)] = (dn, entry)
+
+	def add_as(self, dn):
+		""" Add this entry with the given absolute dn """
 		if self.dn:
-			raise ValueError('self.dn is already set, this means this entry is already part of an LDAP tree. Please consider moving it instead.')
-		self.dn = dn
-		self._ldap.add(dn, self.attrs)
+			raise ValueError('self.dn is set, this means this entry already is part of an LDAP tree.')
+		self._ldap.add(dn, self._cached_attrs)
+
+	def move(self, new_parent):
+		self._ldap.move(self.dn, self.rdn, new_parent.dn)
 
 	def fetch_children(self):
-		self._cached_children = rv = { self.child_name(dn): LMAP(self._con, dn, timeout) for dn, _ in self._ldap.search(self.dn, ldap.SCOPE_ONELEVEL, attrlist=[], timeout=self.timeout).items() }
+		self._cached_children = rv = { _child_name(dn): lmap(ldap=self._ldap, dn=dn, timeout=self.timeout) for dn, _ in self._ldap.search(self.dn, ldap.Scope.ONELEVEL, attrlist=[], timeout=self.timeout) }
 		return rv
 
 	def children(self):
-		return self._cached_children or self.fetch_children()
+		return self._cached_children if self._cached_children is not None else self.fetch_children()
 
 	def __dir__(self):
 		return list(itertools.chain(self.__dict__.keys(), iter(self.children())))
 	
+	def replace(self, childname, newchild):
+		pass #FIXME
+
 	#CAUTION: The behavior of this operation is a bit non-standard.
 	def __setattr__(self, name, value):
 		""" Add, delete or move an LDAP entry
 			
 			You may add a child LDAP entry by calling
-				new_entry = LMAP()
+				new_entry = lmap()
 				existing_entry.new_child = new_entry
 			You may move an LDAP entry by calling
 				existing_entry.new_child = another_existing_entry
 			Finally, you can recursively delete a child by calling
 				existing_entry.existing_child = None
 		"""
-		if isinstance(value, LMAP):
+		if isinstance(value, lmap):
 			if value is None: # Recursively delete child
 				self.__delattr__(name)
 			else: # Add or move entry
@@ -94,10 +149,14 @@ class LMAP(dict):
 				if value.dn: # Move entry
 					value.move(self)
 				else: # Add entry
-					value.add( name + self.dn )
+					#FIXME really statically use ou here?
+					self.add('ou='+name, value)
 		self.__dict__[name] = value
 
 	def __getattr__(self, name):
+		if name == 'rdn':
+			#FIXME is this correct?
+			return self.dn.partition(',')[0]
 		if name in self.children().keys():
 			return self._cached_children[name]
 		raise AttributeError(name)
@@ -108,6 +167,10 @@ class LMAP(dict):
 
 	def __delattr__(self, name):
 		""" Recursively delete an entry """
+		if not self._cached_children:
+			self.fetch_children()
+		if not name in self._cached_children:
+			raise AttributeError('Cannot delete {}: There is no such attribute in here!'.format(name))
 		dn, child = self._cached_children[name]
 		for name in child.children().keys():
 			child.__delattr__(name)
@@ -115,9 +178,9 @@ class LMAP(dict):
 		self._ldap.delete(dn)
 
 	def __call__(self, *args):
-		return [ LMAP(self._con, dn, timeout) for dn, _ in self._ldap.search_st(self.dn, ldap.SCOPE_SUBTREE, filter=*args, attrlist=[], timeout=self.timeout).items() ]
+		return [ lmap(ldap=self._ldap, dn=dn, timeout=timeout) for dn, _ in self._ldap.search_st(self.dn, ldap.Scope.SUBTREE, filter=args, attrlist=[], timeout=self.timeout).items() ]
 
 #Auxiliary stuff
 	def __str__(self):
-		return dn
+		return "<'{}': {} with {}>".format(self.dn, str(self._cached_attrs), str(self.children()))
 
---
+++ lmap/test_lmap.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+import sys,os,time
+from test_ldap import BASE_DN, py_test_object
+from unittest import TestCase, mock, main
+from ldap import ldap, ldapmod
+from lmap import lmap
+
+class SlapdLmapTest(TestCase):
+	def setUp(self):
+		#super(self, SlapdLmapTest).setUp(self)
+		self.ldap = mock.Mock(spec=ldap)
+		res = py_test_object.copy()
+		self.ldap.search.return_value = [(res['dn'], res)]
+		self.lmap = lmap(py_test_object.copy(), BASE_DN, self.ldap)
+	
+	def testAdd(self):
+		""" Add an object and see whether the data ends up in the database """
+		attrs = py_test_object
+		self.lmap.foo = lmap(attrs)
+		self.ldap.add.assert_called_with('ou=foo,'+BASE_DN, attrs)
+	
+	def testAttributeAdd(self):
+		""" Add an attributes to an existing object via object[attribute] = value """
+		self.lmap['test'] = 'foo'
+		self.ldap.modify.assert_called_with(BASE_DN, [(ldapmod.ADD, 'test', 'foo')])
+		self.assertIn('test', self.lmap._cached_attrs)
+		self.assertEqual(self.lmap._cached_attrs['test'], 'foo')
+
+	def testAttributeReplace(self):
+		""" Replace an attribute of an existing object via object[attribute] = value """
+		self.lmap['uid'] = 'foobar'
+		self.ldap.modify.assert_called_with(BASE_DN, [(ldapmod.REPLACE, 'uid', 'foobar')])
+		self.assertIn('uid', self.lmap._cached_attrs)
+		self.assertEqual(self.lmap._cached_attrs['uid'], 'foobar')
+
+	def testAttributeDelete(self):
+		""" Delete an attribute of an existing object via object[attribute] = None """
+		del self.lmap['uid']
+		self.ldap.modify.assert_called_with(BASE_DN, [(ldapmod.DELETE, 'uid', None)])
+		self.assertNotIn('uid', self.lmap._cached_attrs)
+
+	def testAttributeAccess(self):
+		""" Test attribute access via object[attribute] """
+		self.assertEqual(self.lmap['uid'], 'guest')
+		self.assertNotIn('this_attrbite_should_not_exist', self.lmap)
+		with self.assertRaises(KeyError):
+			self.lmap['this_attribute_should_not_exist']
+
+	def testTransactionLogic(self):
+		""" Start a transaction, do some stuff and look whether the changes are recorded properly and whether the log is removed correctly on an error """
+		with self.lmap as l:
+			# add an attribute
+			self.lmap['test'] = 'foo'
+			self.assertIn('test', self.lmap)
+			self.assertEqual(self.lmap['test'], 'foo')
+			# replace an attribute
+			self.lmap['uid'] = 'foobar'
+			self.assertIn('uid', self.lmap)
+			self.assertEqual(self.lmap['uid'], 'foobar')
+			# remove an attribute
+			del self.lmap['cn']
+			self.assertNotIn('cn', self.lmap)
+			with self.assertRaises(KeyError):
+				self.lmap['cn']
+		expected_mods = [(ldapmod.DELETE, 'cn', None), (ldapmod.REPLACE, 'uid', 'foobar'), (ldapmod.ADD, 'test', 'foo')]
+		self.assertEqual(self.ldap.modify.call_args[0][0], BASE_DN)
+		self.assertCountEqual(self.ldap.modify.call_args[0][1], expected_mods)
+	
+	def testDelete(self):
+		""" Try deleting an object via parent.delete(childname) """
+		attrs = py_test_object.copy()
+		#This is tested above FIXME fully separate these two tests
+		self.lmap.foo = lmap(attrs)
+		self.lmap.foo._cached_children = {}
+		del self.lmap.foo
+		self.ldap.delete.assert_called_with('ou=foo,'+BASE_DN)
+		
+if __name__ == '__main__':
+	main()
+
